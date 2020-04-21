33f1e5571ac1ac796a3b5910803e9572e8611d65
KHG <skidrom1@gmail.com>
2020-04-21 12:05:45 +0900

    delete

--- capstone/capstone_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from capstone_app.models import Test
-# Register your models here.
-
-
-admin.site.register(Test)
--- capstone/capstone_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CapstoneAppConfig(AppConfig):
-    name = 'capstone_app'
