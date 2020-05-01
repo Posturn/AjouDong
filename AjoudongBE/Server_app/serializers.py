0a5201fe6ab0f2268b668dbfbd2b5caa8ef32b4a
mistercle <mistercle@gmail.com>
2020-05-01 17:32:13 +0900

    api 실험

--- AjoudongBE/Server_app/serializers.py
+++ AjoudongBE/Server_app/serializers.py
@@ -1,7 +1,11 @@
-from rest_framework import serializers
+from rest_framework import serializers, viewsets
 from Server_app.models import userAccount
 
 class TestSerializer(serializers.ModelSerializer):
     class Meta:
         model=userAccount
         fields=('uID', 'uPW', )
+
+class TestViewSet(viewsets.ModelViewSet):
+    queryset = userAccount.objects.all()
+    serializer_class = TestSerializer
