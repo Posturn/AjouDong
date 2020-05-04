585b867f82103c2751ba30a109bae486f3eb8282
mistercle <mistercle@gmail.com>
2020-05-04 13:14:03 +0900

    userAccount managerAccount 추가

--- AjoudongBE/Server_app/models.py
+++ AjoudongBE/Server_app/models.py
@@ -9,3 +9,17 @@ class userAccount(models.Model):
     # age = models.IntegerField(default=0)
     uID = models.CharField(max_length=20)
     uPW = models.IntegerField()
+    uIMG = models.CharField(max_length=128)
+    uName = models.CharField(max_length=10)
+    uJender = models.BooleanField()
+    uSchoolID = models.IntegerField(private_key=True)#private key
+    uMajor = models.CharField(max_length=32)
+    uPhoneNumber = models.IntegerField()
+    uCollege = models.CharField(max_length=32)
+
+class managerAccount(models.Model):
+    mID = models.CharField(max_length=20, private_key=True)#private key
+    clubID = models.IntegerField()
+    clubName = models.CharField(5)
+    clubIMG = models.CharField(128)
+    newbieAlarm = models.BooleanField()
--- AjoudongBE/Server_app/views.py
+++ AjoudongBE/Server_app/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 
 from Server_app.models import userAccount
 from Server_app.serializers import TestSerializer
 
+
 class TestViewSet(viewsets.ModelViewSet):
     queryset=userAccount.objects.all()
     serializer_class=TestSerializer
+
+class userAccountViewSet(ListAPIView):
+    queryset = userAccount.objects.all()
+    serializer_class = Testserializer
