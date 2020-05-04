1d451afd20f4823f069b5b18cafe81894e6f9c16
mistercle <mistercle@gmail.com>
2020-05-04 13:48:24 +0900

    모델 수정

--- AjoudongBE/Server_app/views.py
+++ AjoudongBE/Server_app/views.py
@@ -7,11 +7,10 @@ from rest_framework.generics import ListAPIView
 from Server_app.models import userAccount
 from Server_app.serializers import TestSerializer
 
-
 class TestViewSet(viewsets.ModelViewSet):
     queryset=userAccount.objects.all()
     serializer_class=TestSerializer
 
 class userAccountViewSet(ListAPIView):
     queryset = userAccount.objects.all()
-    serializer_class = Testserializer
+    serializer_class = TestSerializer
