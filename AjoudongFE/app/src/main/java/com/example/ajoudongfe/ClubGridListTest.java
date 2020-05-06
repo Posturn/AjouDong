e0719527d97fdac1045565a6a0b0301afe97e64d
KHG <skidrom1@gmail.com>
2020-05-07 00:05:40 +0900

    account + clublist 머지 버전

---
+++ AjoudongFE/app/src/main/java/com/example/ajoudongfe/ClubGridListTest.java
@@ -0,0 +1,31 @@
+package com.example.ajoudongfe;
+
+import android.graphics.drawable.Drawable;
+
+public class ClubGridListTest {
+    private Drawable mThumbIds;
+    String name;
+
+    public ClubGridListTest(Drawable mThumbIds, String name)
+    {
+        this.mThumbIds = mThumbIds;
+        this.name = name;
+
+    }
+
+    public Drawable getmThumbIds() {
+        return mThumbIds;
+    }
+
+    public void setmThumbIds(Drawable mThumbIds) {
+        this.mThumbIds = mThumbIds;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
