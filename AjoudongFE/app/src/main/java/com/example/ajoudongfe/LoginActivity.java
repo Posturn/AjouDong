74895ed6713e4568fff273b17f33005dd85c4908
mistercle <mistercle@gmail.com>
2020-05-06 15:58:29 +0900

    더미 함수 생성

--- AjoudongFE/app/src/main/java/com/example/ajoudongfe/LoginActivity.java
+++ AjoudongFE/app/src/main/java/com/example/ajoudongfe/LoginActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,30 +14,72 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity
 {
+    Button loginButton;
+    TextInputLayout idLayout;
+    TextInputLayout pwLayout;
+    TextInputEditText idText;
+    TextInputEditText pwText;
+    Chip autoLogin;
+    TextView findID;
+    TextView findPW;
+    TextView signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        TextInputLayout idLayout = (TextInputLayout) findViewById(R.id.idLayout);
-        TextInputLayout pwLayout = (TextInputLayout) findViewById(R.id.pwLayout);
-        TextInputEditText idText = (TextInputEditText) findViewById(R.id.idInputText);
-        TextInputEditText pwText = (TextInputEditText) findViewById(R.id.pwInputText);
-        Chip autoLogin = (Chip) findViewById(R.id.autoLogin);
-        TextView findID = (TextView)findViewById(R.id.findID);
-        TextView findPW = (TextView)findViewById(R.id.findPW);
-        TextView signup = (TextView)findViewById(R.id.signup);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        idLayout = (TextInputLayout) findViewById(R.id.idLayout);
+        pwLayout = (TextInputLayout) findViewById(R.id.pwLayout);
+        idText = (TextInputEditText) findViewById(R.id.idInputText);
+        pwText = (TextInputEditText) findViewById(R.id.pwInputText);
+        autoLogin = (Chip) findViewById(R.id.autoLogin);
+        findID = (TextView)findViewById(R.id.findID);
+        findPW = (TextView)findViewById(R.id.findPW);
+        signup = (TextView)findViewById(R.id.signup);
+
+        loginButton.setClickable(true);
+        findID.setClickable(true);
+        findPW.setClickable(true);
+        signup.setClickable(true);
+
 
         loginButton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view)
             {
+                String ID = idText.getText().toString();
+                String PW = pwText.getText().toString();
 
+                Toast.makeText(getApplicationContext(), ID, Toast.LENGTH_LONG).show();
             }
         });
 
+        findID.setOnClickListener(new TextView.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "아이디 찾기", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        findPW.setOnClickListener(new TextView.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "비밀번호 찾기", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        signup.setOnClickListener(new TextView.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Toast.makeText(getApplicationContext(), "회원가입", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
 
