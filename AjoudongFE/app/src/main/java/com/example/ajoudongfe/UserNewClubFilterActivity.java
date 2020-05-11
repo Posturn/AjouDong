bbe63cc4419fee00d029879e36a59ae63b36397d
dykim <do02reen24@gmail.com>
2020-05-11 20:07:25 +0900

    각 페이지의 필터 엑티비티 및 xml생성

---
+++ AjoudongFE/app/src/main/java/com/example/ajoudongfe/UserNewClubFilterActivity.java
@@ -0,0 +1,14 @@
+package com.example.ajoudongfe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserNewClubFilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_new_club_filter);
+    }
+}
