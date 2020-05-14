bc33bd3681696f67f9fc9c94536bccf39a0da2d7
mistercle <mistercle@gmail.com>
2020-05-14 14:55:00 +0900

    단과대 및 학과 파일 생성

--- AjoudongFE/app/src/main/java/com/example/ajoudongfe/SignupActivity.java
+++ AjoudongFE/app/src/main/java/com/example/ajoudongfe/SignupActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Base64;
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
@@ -17,10 +20,18 @@ import retrofit2.Call;
 
 public class SignupActivity extends AppCompatActivity {
 
+    TextInputEditText idText;
+    TextInputEditText pwText;
+    TextInputEditText nameText;
+    TextInputEditText shoolIDText;
+    Spinner
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
     }
 
 }
