f9da7c0c874d48500658c00eb85566f43868add5
dykim <do02reen24@gmail.com>
2020-05-29 17:01:56 +0900

    ajoudong 프론트엔드

--- AjoudongFE/app/src/main/java/com/example/ajoudongfe/UserMyAjouDongActivity.java
+++ AjoudongFE/app/src/main/java/com/example/ajoudongfe/UserMyAjouDongActivity.java
@@ -1,14 +1,107 @@
 package com.example.ajoudongfe;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.ParseException;
+
+import retrofit2.Retrofit;
 
 public class UserMyAjouDongActivity extends AppCompatActivity {
+
+    private static String BASE_URL= "http://10.0.2.2:8000";
+    private ArrayAdapter<CharSequence> majorAdapter;
+    private Retrofit retrofit;
+
+    private EditText userphone;
+
+    private Spinner collegeSpinner;
+    private Spinner majorSpinner;
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_my_ajou_dong);
+
+        userphone = (EditText) findViewById(R.id.edit_phone);
+        userphone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+        collegeSpinner = (Spinner)findViewById(R.id.collegeSpinner);
+        majorSpinner = (Spinner)findViewById(R.id.majorSpinner);
+
+        toolbar = (Toolbar) findViewById(R.id.ajoudongtoolbar);     //툴바 생성
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();    //뒤로가기 버튼 생성
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        collegeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(!(collegeSpinner.getItemAtPosition(i).equals("--단과대학교선택--")))
+                {
+                    int resid = getResources().getIdentifier(collegeSpinner.getItemAtPosition(i).toString(), "array", getPackageName());
+//                    uCollege = collegeSpinner.getItemAtPosition(i).toString();
+                    majorAdapter = ArrayAdapter.createFromResource(getApplicationContext(), resid, android.R.layout.simple_spinner_dropdown_item);
+                    majorSpinner.setAdapter(majorAdapter);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //아무것도 하지않음
+            }
+        });
+
+        majorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                uMajor = majorSpinner.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //아무것도 하지않음
+            }
+        });
+
+
+    }
+
+    @Override       //저장 버튼 생성
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.manager_club_info_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {       //뒤로가기, 저장 버튼 기능 구현
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_btn1:      //저장하기 버튼
+                Toast.makeText(getApplicationContext(), "저장되었습니다!", Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
