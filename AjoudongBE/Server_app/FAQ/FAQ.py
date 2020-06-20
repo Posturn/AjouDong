d06e35e7bccb284919aa412330279281f378c37a
mistercle <mistercle@gmail.com>
2020-06-20 22:54:52 +0900

    FAQ get부분 수정

--- AjoudongBE/Server_app/FAQ/FAQ.py
+++ AjoudongBE/Server_app/FAQ/FAQ.py
@@ -7,6 +7,8 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse, JsonResponse, FileResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+import datetime
 
 # Create your views here.
 from rest_framework import viewsets
@@ -18,75 +20,88 @@ class postFAQ(View):
     @csrf_exempt
     def post(self, request):
         data = json.loads(request.body)
+        print(data)
         try:
-            clubID = data['clubID']
-            userID = data['uID']
-            FAQDate = data['datetime']
-            isAnonymous = data['isAnonymous']
-            FAQContent = data['FAQContent']
+            now = timezone.now()
+            
+            FAQ.objects.create(
+                userID = data['uID'],
+                clubID_id = data['clubID'],
+                FAQDate = now,
+                isAnonymous = data['isAnonymous'],
+                FAQContent = data['FAQContent']
+            ).save()
 
             return JsonResponse({'response' : 1}, status = 200)
 
         except KeyError:
-            return JsonResponse({'response' : -2}, status = 401)
+            return JsonResponse({'response' : -2}, status = 400)
 
 class postFAQComment(View):
     @csrf_exempt
     def post(self, request):
         data = json.loads(request.body)
+        print(data)
         try:
-            clubID = data['clubID']
-            userID = data['uID']
-            FAQCommentDate = data['datetime']
-            isAnonymous = data['isAnonymous']
-            FAQCommentContent = data['FAQCommentContent']
+
+            now = timezone.now()
+            
+            FAQComment.objects.create(
+                userID = data['uID'],
+                clubID_id = data['clubID'],
+                FAQID_id = data['FAQID'],
+                FAQCommentDate = now,
+                isAnonymous = data['isAnonymous'],
+                FAQCommentContent = data['FAQCommentContent']
+            ).save()
 
             return JsonResponse({'response' : 1}, status = 200)
 
         except KeyError:
-            return JsonResponse({'response' : -2}, status = 401)
+            return JsonResponse({'response' : -2}, status = 400)
 
 class getFAQ(View):
     @csrf_exempt
     def get(self, request, clubID):
         try:
             queryset = FAQ.objects.filter(clubID_id=clubID).order_by('FAQDate')
-            FAQList = list(queryset.values())
-            print(FAQList)
+            FAQs = list(queryset.values())
+            print(FAQs)
             FAQList = []
-            for i in FAQist:
+            for i in FAQs:
+                print(i)
                 FAQInfo = {}
-                User = UserAccount.objects.get(uSchoolID = i.userID)
-                FAQInfo['userID'] = i.userID
+                user = UserAccount.objects.get(uSchoolID = i['userID'])
+                FAQInfo['userID'] = i['userID']
                 FAQInfo['uName'] = user.uName
-                FAQInfo['FAQContent'] = i.FAQContent
-                FAQInfo['FAQDate'] = i.FAQDate
-                FAQInfo['isAnonymous'] = i.isAnonymous
-                CommentsQueryset = FAQComment.objects.filter(FAQID_id=i.FAQID).order_by('FAQDate')
+                FAQInfo['FAQContent'] = i['FAQContent']
+                FAQInfo['FAQDate'] = i['FAQDate']
+                FAQInfo['isAnonymous'] = i['isAnonymous']
+                CommentsQueryset = FAQComment.objects.filter(FAQID_id=i['FAQID']).order_by('FAQCommentDate')
                 comments = list(CommentsQueryset.values())
                 commentList = []
-
+                
                 for comment in comments:
                     commentInfo = {}
-                    if(comment.userID > 9999):
-                        commentUser = UserAccount.objects.get(uSchoolID = comment.userID)
-                        commentInfo['userID'] = comment.userID
+                    if(comment['userID'] > 9999):
+                        commentUser = UserAccount.objects.get(uSchoolID = comment['userID'])
+                        commentInfo['userID'] = comment['userID']
                         commentInfo['uName'] = commentUser.uName
                     else:
-                        commentUser = Club.objects.get(clubID = comment.userID)
-                        commentInfo['userID'] = comment.clubID
+                        commentUser = Club.objects.get(clubID = comment['userID'])
+                        commentInfo['userID'] = comment['userID']
                         commentInfo['uName'] = commentUser.clubID
 
-                    commentInfo['FAQCommentContent'] = comment.FAQCommentContent
-                    commentInfo['FAQCommentDate'] = comment.FAQCommentDate
-                    commentInfo['isAnonymous'] = comment.isAnonymous
+                    commentInfo['FAQCommentContent'] = comment['FAQCommentContent']
+                    commentInfo['FAQCommentDate'] = comment['FAQCommentDate']
+                    commentInfo['isAnonymous'] = comment['isAnonymous']
                     commentList.append(commentInfo)
 
                 FAQInfo['FAQComments'] = commentList
                 FAQList.append(FAQInfo)
 
             
-            return JsonResponse({'response' : 1, content : FAQList}, status = 200)
+            return JsonResponse({'response' : 1, 'content' : FAQList}, status = 200)
 
         except KeyError:
             return JsonResponse({'response' : -2}, status = 401)
