63a172a16fc9043badc28685e929f783dab13957
mistercle <mistercle@gmail.com>
2020-06-21 17:38:39 +0900

    비밀번호 찾기 백엔드 완성

---
+++ AjoudongFE/app/src/main/java/com/example/ajoudongfe/FindPWActivity.java
@@ -0,0 +1,13 @@
+package com.example.ajoudongfe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindPWActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_p_w);
+    }
+}
